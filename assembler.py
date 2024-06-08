ce9b42ba0cb13d9ebb022b7d3a0c7f8280979707
21KennethTran <kenpt03@gmail.com>
2024-06-08 12:51:42 -0700

    started on machine code conversion

---
+++ assembler.py
@@ -0,0 +1,33 @@
+opcode = {"lwr":"0000", "swr":"0001", "mov":"0010", "mof":"0011", "and":"0100", "or":"0101", "xor":"0110", "add":"0111",
+          "sub":"1000", "lsl":"1001", "rsl":"1010", "cmp":"1011", "beq":"1100", "bne":"1101", "j":"1110", "don":"1111"}
+
+operand = {"lwr":"0000", "swr":"0001", "mov":"0010", "mof":"0011", "and":"0100", "or":"0101", "xor":"0110", "add":"0111",
+          "sub":"1000", "lsl":"1001", "rsl":"1010", "cmp":"1011", "beq":"1100", "bne":"1101", "j":"1110", "don":"1111"}
+
+with open("code.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        final = ""
+        parts = line.split(" ")
+        final += operand[parts[0]]
+        if parts[0] == 'j':
+            #write code to jump to a specific point
+        elif parts[0] == 'bne':
+            #write code to jump to a specific point
+        elif parts[0] == 'beq':
+            #write code to jump to a specific point
+        elif parts[0] == 'bgt':
+            #write code to jump to a specific point
+        elif parts[0] == 'ble':
+            #write code to jump to a specific point
+        else:
+            #get register
+            register = int(parts[1][2:])
+            register = bin(register)[2:]
+            final += str(register)
+            # get immediate value from lookup table
+            if parts[0] == 'add' or parts[0] == 'sub':
+                #add index of thing in lookup table
+        mach_code = open("mach_code.txt", "w")
+        mach_code.write(final)
+        file.close()
