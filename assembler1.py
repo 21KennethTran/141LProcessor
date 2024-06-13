ae2037032839631cf3f75cf8d17f61d0518d5868
21KennethTran <kenpt03@gmail.com>
2024-06-13 04:08:34 -0700

    assembler1 can convert instr into machine code

--- assembler1.py
+++ assembler1.py
@@ -1,30 +1,45 @@
-operand = {"lwr*":"0000","swr":"0001","mov":"0010","mof*":"0011","and*":"0100","orr*":"0101","xor*":"0110","add*":"0111","sub*":"1000","lsl*":"1001","rsl*":"1010", "cmp*":"1011", "beq":"1100", "bne":"1101", "j":"1110", "done":"1111"}
+opcode = {"lwr":"0000", "str":"0001", "mov":"0010", "mof":"0011", "and":"0100", "or":"0101", "xor":"0110", "add":"0111",
+          "sub":"1000", "lsl":"1001", "rsl":"1010", "cmp":"1011", "beq":"1100", "bne":"1101", "j":"1110", "don":"1111"}
+
+operand = {"$t0":"0000", "$t1":"0001", "$t2":"0010", "$t3":"0011", "$t4":"0100", "$t5":"0101", "$t6":"0110", "$t7":"0111",
+          "$t8":"1000", "$t9":"1001", "$t10":"1010", "$t11":"1011", "$t12":"1100", "$t13":"1101", "$t14":"1110", "$t15":"1111"}
+
+Lookup = {"$t0":"0000", "$t1":"0001", "$t2":"0010", "$t3":"0011", "$t4":"0100", "$t5":"0101", "$t6":"0110", "$t7":"0111",
+          "$t8":"1000", "$t9":"1001", "$t10":"1010", "$t11":"1011", "$t12":"1100", "$t13":"1101", "$t14":"1110", "$t15":"1111"}
+def assembleLine(part):
+    final = ""
+
+    opc = part[0].lower()
+
+    if opc.endswith('*'):
+        opc = opc.strip('*')
+        if opc not in opcode:
+            return None
+        final += opcode[opc] + "1"
+    else:
+        if opc not in opcode:
+            return None
+        final += opcode[opc] + "0"
+
+    ope = part[1]
+    if ope in operand:
+        final += operand[ope]
+    else:
+        final += ope # deal with jump mapping later
+    
+    return final
 
 with open("code2.txt", "r") as file:
     lines = file.readlines()
+    full = []
     for line in lines:
-        final = ""
-        parts = line.split(" ")
-        final += operand[parts[0]]
-        if parts[0] == 'j':
-            
-        elif parts[0] == 'bne':
-            #write code to jump to a specific point
-        elif parts[0] == 'beq':
-            #write code to jump to a specific point
-        elif parts[0] == 'bgt':
-            #write code to jump to a specific point
-        elif parts[0] == 'ble':
-            #write code to jump to a specific point
-        else:
-            #get register
-            register = int(parts[1][2:])
-            register = bin(register)[2:]
-            final += str(register)
-            # get immediate value from lookup table
-            if parts[0] == 'add' or parts[0] == 'sub':
-                #add index of thing in lookup table
-        mach_code = open("mach_code.txt", "w")
-        mach_code.write(final)
-        file.close()
+        line = line.split('#')[0].strip()
+        if line:
+            part = line.split()
+            instr = assembleLine(part)
+            if instr is not None:
+                full.append(instr)
 
+with open("machcode2.txt", "w") as out:
+        for instruction in full:
+            out.write(instruction + '\n')
